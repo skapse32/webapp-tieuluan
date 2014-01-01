435b7553dd7dba3f0e7bd1a44193975712242de4
hongphongcntt <hongphongcntt@gmail.com>
2014-01-01 08:30:48 +0000

    sanphamnew edit

--- AuctionWebService/src/util/SanphamComparatorNew.java
+++ AuctionWebService/src/util/SanphamComparatorNew.java
@@ -1,5 +1,6 @@
 package util;
 
+import java.sql.Timestamp;
 import java.util.Comparator;
 
 import dao.Sanpham;
@@ -8,7 +9,13 @@ public class SanphamComparatorNew implements Comparator<Sanpham>{
 
 	@Override
 	public int compare(Sanpham o1, Sanpham o2) {
-		return 1;
+		if(o1.getThoigianbatdau().after(o2.getThoigianbatdau())){
+			return -1;
+		}
+		if(o2.getThoigianbatdau().after(o1.getThoigianbatdau())){
+			return 1;
+		}
+		return 0;
 	}
 
 }
