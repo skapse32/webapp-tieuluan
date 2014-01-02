2f26d5aa625ab69b2d611a8a7637c766107033ed
hieugiang92 <hieugiang92@gmail.com>
2014-01-02 19:10:36 +0000

    UTF-8 OK

--- AuctionWebService/src/function/Server.java
+++ AuctionWebService/src/function/Server.java
@@ -2,7 +2,7 @@ package function;
 
 public class Server {
 
-	public static String addressAuthenWS="http://localhost:8080/auth/services/";
-	public static String saveDirectory="C:\\Users\\VANHOA\\Documents\\workspace-sts-3.3.0.RELEASE\\TieuLuan\\.metadata\\.plugins\\org.eclipse.wst.server.core\\tmp3\\wtpwebapps\\AuctionWebService\\resources\\images\\product\\";
-	
+	public static String addressAuthenWS="http://112.78.8.184:8080/auth/services/";
+	//public static String addressAuthenWS="http://112.78.8.184/:8080/auth/services/";
+	public static String saveDirectory="C:\\Users\\GIANG\\Documents\\workspace-sts-3.3.0.RELEASE\\AuctionWebService\\WebRoot\\resources\\images\\product\\";
 }
